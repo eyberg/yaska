11deb3594bf66b67df8b18e58e0fc8587846b0c1
ian eyberg <ian@telematter.com>
2009-06-05 10:34:48 -0500

    getting this prepped up for the debug mode

--- yaska.rb
+++ yaska.rb
@@ -1,10 +1,18 @@
 require 'open3'
 
 class Yaska
+  attr_accessor :cwcount
+  attr_accessor :rwcount
 
-  @@GRAMMARDIR = "test"
+  @@GRAMMARDIR = "grammars"
   @@GRAMMARFILE = "Test.g"
-  @@TESTFILE = "com.yaska.TestYaska"  # make sure to include the package name if applicable
+  @@PACKAGE = "com.yaska"
+  @@TESTFILE = "TestYaska"
+
+  def initialize
+    @cwcount = 0
+    @rwcount = 0
+  end
 
   # compile the grammar, spit out any warnings
   # return the warning count
@@ -28,78 +36,82 @@ class Yaska
   # check for file changes every 2 seconds
   # compile, test, check for any changes
   def start
-  cwcount = 0
-  rwcount = 0
+    ttop = nil    # last unit-test modification-time
+    gtop = nil    # last grammar modification-time
 
-  last = nil
+    loop do
+      tcurrent = File.mtime "#{@@TESTFILE}.java"
+      gcurrent = File.mtime "#{@@GRAMMARDIR}/#{@@GRAMMARFILE}"
 
-  loop do
-    current = File.mtime "#{@@GRAMMARDIR}/#{@@GRAMMARFILE}"
-
-    if last.nil? || last < current
-    
-      comwarn = compile
-
-      # run
-      stdin, stdout, stderr = Open3.popen3("java org.junit.runner.JUnitCore #{@@TESTFILE}")
-
-      # output stdout (errors)
-      oput = []
-      stdout.each do |line| oput << line end
-      sout = oput.join
-
-      oput.each do |line|
-        puts line
+      if ttop.nil? || ttop < tcurrent || gtop < gcurrent
+        runtests
+        gtop = gcurrent
+        ttop = tcurrent
       end
 
-      # out put stderr (warnings)
-      eput = []
-      stderr.each do |line| eput << line end
+      sleep 2
+    end
+  end
 
-      runwarn = eput.count
-      eput.each do |line|
-        puts line
-      end
 
-      # testing errors
-      if sout.match("FAILURES!!!") then
-        puts "\033[1m\033[31m\033[40m Broke a Test! \033[0m"
-      else
-        puts "\033[1m\033[32m\033[40m Passing all Tests! \033[0m"
-      end
+  # compile and run the tests
+  def runtests
+    comwarn = compile
 
-      # compilation warnings
-      puts "\033[1m\033[30m\033[40m Compile-Time Warnings \033[0m"
-      puts "\033[1m\033[30m\033[40m -----------------\033[0m"
+    # run
+    stdin, stdout, stderr = Open3.popen3("java org.junit.runner.JUnitCore #{@@PACKAGE}.#{@@TESTFILE}")
 
-      if cwcount < comwarn then
-        puts "\033[1m\033[31m\033[40m #{comwarn} Warnings: Generating more! \033[0m"
-      elsif cwcount.eql? comwarn then
-        puts "\033[1m\033[33m\033[40m #{comwarn} Warnings: Generating the same number! \033[0m"
-      else
-        puts "\033[1m\033[32m\033[40m #{comwarn} Warnings: Generating less! \033[0m"
-      end
-      cwcount = comwarn
+    # output stdout (errors)
+    oput = []
+    stdout.each do |line| oput << line end
+    sout = oput.join
 
-      # runtime warnings
-      puts "\033[1m\033[30m\033[40m Runtime Warnings \033[0m"
-      puts "\033[1m\033[30m\033[40m -----------------\033[0m"
-
-      if rwcount < runwarn then
-        puts "\033[1m\033[31m\033[40m #{runwarn} Warnings: Generating more! \033[0m"
-      elsif rwcount.eql? runwarn then
-        puts "\033[1m\033[33m\033[40m #{runwarn} Warnings: Generating the same number! \033[0m"
-      else
-        puts "\033[1m\033[32m\033[40m #{runwarn} Warnings: Generating less! \033[0m"
-      end
-
-      rwcount = runwarn
-
-      last = current
+    oput.each do |line|
+      puts line
     end
 
-    sleep 2
-  end
+    # out put stderr (warnings)
+    eput = []
+    stderr.each do |line| eput << line end
+
+    runwarn = eput.count
+    eput.each do |line|
+      puts line
+    end
+
+    # testing errors
+    if sout.match("FAILURES!!!") then
+      puts "\033[1m\033[31m\033[40m Broke a Test! \033[0m"
+    else
+      puts "\033[1m\033[32m\033[40m Passing all Tests! \033[0m"
+    end
+
+    # compilation warnings
+    puts "\033[1m\033[30m\033[40m Compile-Time Warnings \033[0m"
+    puts "\033[1m\033[30m\033[40m -----------------\033[0m"
+
+    if @cwcount < comwarn then
+      puts "\033[1m\033[31m\033[40m #{comwarn} Warnings: Generating more! \033[0m"
+    elsif @cwcount.eql? comwarn then
+      puts "\033[1m\033[33m\033[40m #{comwarn} Warnings: Generating the same number! \033[0m"
+    else
+      puts "\033[1m\033[32m\033[40m #{comwarn} Warnings: Generating less! \033[0m"
+    end
+    @cwcount = comwarn
+
+    # runtime warnings
+    puts "\033[1m\033[30m\033[40m Runtime Warnings \033[0m"
+    puts "\033[1m\033[30m\033[40m -----------------\033[0m"
+
+    if @rwcount < runwarn then
+      puts "\033[1m\033[31m\033[40m #{runwarn} Warnings: Generating more! \033[0m"
+    elsif @rwcount.eql? runwarn then
+      puts "\033[1m\033[33m\033[40m #{runwarn} Warnings: Generating the same number! \033[0m"
+    else
+      puts "\033[1m\033[32m\033[40m #{runwarn} Warnings: Generating less! \033[0m"
+    end
+
+    @rwcount = runwarn
 
   end
 
